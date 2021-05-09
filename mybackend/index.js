531a0a64c87b109f5cc33719933366812845b02e
MPodgorni <michalpodgorni@yahoo.com>
2021-05-09 14:00:21 +0200

    cleaning

--- mybackend/index.js
+++ mybackend/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const cors = require('cors');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const redis = require('redis');
 const { request } = require('express');
@@ -33,16 +34,96 @@ pgClient.on('error', () => {
 });
 
 pgClient
-.query('CREATE TABLE IF NOT EXISTS games (name TEXT);')
+.query('CREATE TABLE IF NOT EXISTS games (id serial primary key, name TEXT, price decimal(6,2));')
 .catch( (err) => {
     console.log('pgClient error:' + err);
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
+app.get('/', (req, res) => {
+    res.send("Welcome! Devipod server is running!");
 });
 
-const PORT = 5000;
+app.get("/games", (req, res) => {
+    pgClient.query('select * from games order by 1 desc;', (err, result) => {
+        if(err) throw error;
+        res.status(200).json(result.rows);
+    })
+});
+
+app.get("/game/:id", (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+        const textId = `/game/:${id}`;
+        
+        redisClient.get(textId, async (error, result) => {
+            if(result) {
+                return res.status(200).send({
+                    data: JSON.parse(result),
+                    msg: 'Cache.'
+                })
+            } else {
+                pgClient.query('select * from games where id = $1;', [id], (err, result) => {
+                    if(err) throw error;
+
+                    const rows = JSON.stringify(result.rows);
+                    console.log(`id: ${id}, data: ${rows}`);
+
+                    redisClient.set(textId, rows, 'EX', 60 * 60 * 24);
+                    return res.status(200).send({
+                        data: JSON.parse(rows),
+                        msg: 'Server.'
+                    })
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.post('/game', (req, res) => {
+    const { name, price } = req.body;
+
+    pgClient.query('insert into games (name, price) values ($1, $2) returning id;', [name, price], (err, result) => {
+        if(err) throw error;
+        
+        const id = result.rows[0].id;
+        const textId = `/game/:${id}`;
+
+        redisClient.set(textId, JSON.stringify([{id: id, ...req.body}]), 'EX', 60 * 60 * 24);
+        res.status(201).json({
+            msg: 'Success. You add record to db.',
+            data: {id: result.rows[0].id, name: name, price: price}
+        });
+    })
+});
+
+app.delete('/game/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    pgClient.query('delete from games where id = $1;', [id], (err, result) => {
+        if(err) throw error;
+
+        res.status(201).json({
+            msg: 'OK. You delete record from db.',
+        });
+    })
+});
+
+app.put('/game/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { name } = req.body;
+
+    pgClient.query(`update games set name = $1 where id = ${id};`, [name], (err, result) => {
+        if(err) throw error;
+
+        res.status(201).json({
+            msg: 'OK. You update record in db.'
+        });
+    })
+});
+
+const PORT = 4000;
 
 app.listen(PORT, () => {
     console.log(`API listening on port ${PORT}`);
---
+++ myfrontend/src/DeleteById.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const DeleteById = (props) => {
+
+	function deleteGameById() {
+		let gameId = prompt('Podaj id gry:');
+		if (gameId == null) { return };
+		axios.delete(`/api/game/${gameId}`)
+			.then(response => response)
+			.catch(error => console.log(error));
+	}
+
+	return (
+		<>
+			<div>
+				<button onClick={deleteGameById}>Usuń gre (po id)</button>
+			</div>
+		</>
+	);
+};
+
+export default DeleteById;
